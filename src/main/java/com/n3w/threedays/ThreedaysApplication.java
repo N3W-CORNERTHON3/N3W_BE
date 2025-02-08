df0442293465f56c2fd19025e19b22d87dda6f60
JMJ <jmjj2010@naver.com>
2025-02-08 23:00:47 +0900

    초기 세팅

---
+++ src/main/java/com/n3w/threedays/ThreedaysApplication.java
@@ -0,0 +1,13 @@
+package com.n3w.threedays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreedaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreedaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n3w/threedays/ThreedaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n3w.threedays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreedaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
