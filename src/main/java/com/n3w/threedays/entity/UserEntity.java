1bf263c1153563a2520a399bf15f7bd4b2e1318a
JMJ <jmjj2010@naver.com>
2025-02-10 17:01:28 +0900

    entity 생성

--- src/main/java/com/n3w/threedays/entity/UserEntity.java
+++ src/main/java/com/n3w/threedays/entity/UserEntity.java
@@ -7,18 +7,18 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserEntity {  // 클래스명 대문자로 변경
+public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;  // Long 타입 ID 추가
+    @Column(nullable = false, unique = true)  // 기본키이자 닉네임으로 사용됨
+    private String id;
 
-    @Column(nullable = false, unique = true)
-    private String name;
-
+    @Column(nullable = false)
     private String password;
-    private String profileImg;
 
-    public UserEntity(String name){
-        this.name = name;
+    private String profileImg = "src/main/resources/static/default/profile.png"; // 기본 프로필 이미지
+
+    public UserEntity(String id, String password) {
+        this.id = id;
+        this.password = password;
     }
 }
