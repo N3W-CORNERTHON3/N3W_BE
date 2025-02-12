2a40308b9c7635a98d60f8de78861f3c40a95cd7
ssxbin <133107887+ssxbin@users.noreply.github.com>
2025-02-12 18:43:20 +0900

    [FEAT] 프로필 수정

--- src/main/java/com/n3w/threedays/security/JwtAuthenticationFilter.java
+++ src/main/java/com/n3w/threedays/security/JwtAuthenticationFilter.java
@@ -13,7 +13,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
 @Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
